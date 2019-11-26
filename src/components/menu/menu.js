9eaeb8f50d3ca987c6f32a11523a365cb562cbb9
Gabriel Yuri Iwazaki <49596753819@info.local>
2019-11-26 17:23:45 -0300

    up

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,7 +4,40 @@ import "../../Assets/css/menu.css"
 function menu(){
     return (
     <div className="menu">
-        <div className="sidebar"></div>
+        <div className="sidebar-container">
+            <ul className="sidebar">
+                <li>
+                    <a href="#">
+                        <div className="account-img"></div>
+                        Minha conta
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div className="doc-fields"></div>
+                        Campos do Document
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div className="views-add"></div>
+                        Cadastro de Views
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div className="doc-list"></div>
+                    Lista de Documents
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div className="doc-details"></div>
+                        Documents Details
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
     );
 }
--- src/pages/notFound/notFound.js
+++ src/pages/notFound/notFound.js
@@ -1,38 +1,10 @@
 import React, { Component } from 'react';
-import "../../Assets/css/notfound.css";
 
 class notFound extends Component {
     render() {
         return (
             <div className="notfound-container">
-                <div className="error">
-                    <div className="wrap">
-                        <div className="404">
-                            <pre>
-                                <code>
-                                    <span className="green">&lt;!</span><span>DOCTYPE html</span><span className="green">&gt;</span>
-                                    <span className="orange">&lt;html&gt;</span>
-                                    <span className="orange">&lt;style&gt;</span>
-                                    <span className="green">everything</span>: <span className="blue">awesome</span>
-                                    <span className="orange">&lt;/style&gt;</span>
-                                    <span className="orange">&lt;body&gt;</span>
-                                    ERROR 404!
-                                    FILE NOT FOUND!
-				                    <span className="comment">&lt;!--The file you are looking for,
-                                    is not where you think it is.--&gt;</span>
-                                    <span className="orange"></span>
-                                    <br />
-                                </code>
-                            </pre>
-                        </div>
-                    </div>
-                    <span className="info">
-                        <br />
-                        <span className="orange">&nbsp;&lt;/body&gt;</span>
-                        <br />
-                        <span className="orange">&lt;/html&gt;</span>
-                    </span>
-                </div>
+                <h1>Não encontrado</h1>
             </div>
         )
     }
